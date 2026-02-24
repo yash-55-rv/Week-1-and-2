f1d02467ab5c3e18cff7945768ab89587146868e
yash-55-rv <yashwanthrachamalla2007@gmail.com>
2026-02-24 15:41:27 +0530

    2nd program

---
+++ src/Markspcm.java
@@ -0,0 +1,12 @@
+public class Markspcm {
+    public static void main(String[] args) {
+        int m , p , c;
+        double avg;
+        m = 94;
+        p = 95;
+        c = 96;
+        avg = (m + p + c)/ 3;
+        System.out.println("Sam's average mark in PCM is 95");
+
+    }
+}
